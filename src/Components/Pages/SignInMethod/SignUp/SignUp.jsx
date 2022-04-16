d30e5d9be2cbce9a64de7a2fa245bf17eacc1ff4
niiyazobug <niiyaz5555@gmail.com>
2022-04-17 03:04:11 +0600

    create user with email and pass method added

--- src/Components/Pages/SignInMethod/SignUp/SignUp.jsx
+++ src/Components/Pages/SignInMethod/SignUp/SignUp.jsx
@@ -1,8 +1,62 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../../../Firebase/firebase.init';
+import Loading from '../../../Shared/Loading/Loading';
 import SocialSignUp from '../SocialSignUp/SocialSignUp';
 
 const SignUp = () => {
+
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth, {sendEmailVerification: true});
+
+    const [updateProfile, updating, errorForUpdateProfile] = useUpdateProfile(auth);
+
+    const navigate = useNavigate()
+
+
+    const navigateLogin = event => {
+        navigate('/login')
+    }
+
+    let errorElement
+    if (error) {
+        errorElement = <div className='border border-danger pt-3 mb-3 col-5 mx-auto text-bold fw-bold' style={{ fontFamily: 'monospace' }}>
+            <p className='text-danger text-center'>Error: {error?.message}</p>
+        </div>
+    }
+
+
+    if (loading || updating) {
+        return <Loading></Loading>
+    }
+
+    if (user) {
+    //     navigate('/home')
+    console.log('user', user);
+    }
+
+
+    const handleSignUp = async event => {
+        event.preventDefault()
+        const name = event.target.name.value
+        const email = event.target.email.value
+        const password = event.target.password.value
+
+        
+           await createUserWithEmailAndPassword(email, password)
+           await updateProfile({ displayName: name  });
+           alert('Updated profile');
+           navigate('/')
+    
+    }
+
+
     return (
         <div>
             <div>
@@ -11,7 +65,7 @@ const SignUp = () => {
 
             <div>
 
-                <form className='grid'>
+                <form onSubmit={handleSignUp} className='grid'>
 
                     <input type="text" name='name' placeholder='Name' required className='border' />
 
@@ -25,7 +79,7 @@ const SignUp = () => {
 
                 </form>
                 <div>
-                    Already have an account ? <Link to='/login'>Login</Link>
+                    Already have an account ? <Link to='/login' onClick={navigateLogin}>Login</Link>
                 </div>
             </div>
 
