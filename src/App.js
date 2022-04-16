7b03e64ba6a3105c7e73139e7249208298c8b67c
niiyazobug <niiyaz5555@gmail.com>
2022-04-17 00:00:07 +0600

    nav done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Shared/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Pages/Home/Home/Home';
+import Services from './Components/Pages/Services/Services';
+import CheckOut from './Components/Pages/CheckOut/CheckOut';
+import Blogs from './Components/Bolgs/Blogs';
+import About from './Components/Pages/About/About';
+import SignUp from './Components/Pages/SignInMethod/SignUp/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Header></Header>
+
+      <Routes>
+
+<Route path='/' element={<Home></Home>}></Route>
+<Route path='/services' element={<Services></Services>}></Route>
+<Route path='/checkout' element={<CheckOut></CheckOut>}></Route>
+<Route path='/blogs' element={<Blogs></Blogs>}></Route>
+<Route path='/about' element={<About></About>}></Route>
+<Route path='/signup' element={<SignUp></SignUp>}></Route>
+
+      </Routes>
     </div>
   );
 }
--- src/Components/Shared/Header/Header.jsx
+++ src/Components/Shared/Header/Header.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import CoustomLink from '../CoustomLink/CoustomLink';
+import {GrServicePlay} from 'react-icons/gr';
+import {FiShoppingBag} from 'react-icons/fi';
+import {BsFillJournalBookmarkFill} from 'react-icons/bs';
+import {RiLoginCircleLine} from 'react-icons/ri';
 
 const Header = () => {
     return (
-        <div>
-            header
-        </div>
+        <nav className=' px-10 '>
+
+            <div className='flex justify-between'>
+
+                <div className='pt-5'>
+                    <Link to='/' className='border-l rounded-bl-md border-b px-2 py-2 font-semibold'><span className='text-3xl font tracking-tighter'>T</span>HE DEVELOPER</Link>
+                </div>
+
+                <div className='flex gap-11'>
+    
+                    <CoustomLink to='/services' className='px-2 py-1'><GrServicePlay className='text-2xl'></GrServicePlay></CoustomLink>
+                    <CoustomLink to='/checkout' className='px-2 py-1'> <FiShoppingBag className='text-2xl'></FiShoppingBag> </CoustomLink>
+                    <CoustomLink to='/blogs' className='px-2 py-1'> <BsFillJournalBookmarkFill className='text-2xl'></BsFillJournalBookmarkFill> </CoustomLink>
+                    <CoustomLink to='/signup' className='px-2 py-1'> <RiLoginCircleLine className='text-2xl'></RiLoginCircleLine> </CoustomLink>
+
+                </div>
+
+                    <div className='pt-7'>
+                    <Link to='/about' className='px-2 py- border-r border-b rounded-br-md font-semibold '>ABOU <span className='text-3xl tracking-tighter'>T</span></Link>
+                    </div>
+                    
+            </div>
+
+        </nav>
     );
 };
 
