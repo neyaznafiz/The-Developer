a039a37455aa5b55db973dde24d847cb256a0829
niiyazobug <niiyaz5555@gmail.com>
2022-04-18 03:44:03 +0600

    dynamic year added in footer

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='border'>
-            <p className='py-8 text-center font-semibold'>Copyright © 2022 The Developer</p>
+            <p className='py-8 text-center font-semibold'>Copyright © {(new Date().getFullYear())} The Developer</p>
         </div>
     );
 }; 
